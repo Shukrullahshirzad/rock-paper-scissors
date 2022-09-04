f1e3b8c6efd81ababdc6860dac6fda853d15b9e3
shukrullah shirzad <shukrullah.shirzad.779@gmail.com>
2022-09-04 11:13:46 +0430

    add show and hide functionality to choices and selection buttons

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const ruleBtn = document.querySelector(".rules-btn");
 const displayRules = document.querySelector(".rules");
 const closeRules = document.querySelector(".close-rules");
+const display = document.querySelector(".display");
+const choiceBtns = document.querySelector(".choice-btns");
+const selection = document.querySelector(".selection");
 const rock = document.querySelector(".rock");
 const paper = document.querySelector(".paper");
 const scissors = document.querySelector(".scissors");
 const updateScore = document.querySelector("#score");
+const playAgain = document.querySelector(".play-again");
 
 
-
+// show and hide rules 
 
 ruleBtn.addEventListener("click", (e)=>{
     displayRules.style.zIndex = "9";
@@ -18,4 +22,18 @@ closeRules.addEventListener("click", (e)=>{
     displayRules.style.zIndex = "-10";
     displayRules.style.opacity = "0";
 
+})
+
+// show selection
+
+display.addEventListener("click", (e)=>{
+    choiceBtns.classList.add("hide");
+    selection.classList.remove("hide");
+
+})
+
+// hide selection
+playAgain.addEventListener("click", (e)=>{
+    choiceBtns.classList.remove("hide");
+    selection.classList.add("hide");
 })
