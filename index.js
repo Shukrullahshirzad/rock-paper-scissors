cd3f80fbe47288a7209e7332afe57c9782591f66
Shukrullah shirzad <shukrullah_shirzad@yahoo.com>
2023-03-07 11:35:06 +0430

    replace the new files with old ones
    I have revisited this project after a while and made major changes to the style and functionality of this game

--- index.js
+++ index.js
@@ -1,39 +1,100 @@
-const ruleBtn = document.querySelector(".rules-btn");
-const displayRules = document.querySelector(".rules");
-const closeRules = document.querySelector(".close-rules");
-const display = document.querySelector(".display");
-const choiceBtns = document.querySelector(".choice-btns");
-const selection = document.querySelector(".selection");
-const rock = document.querySelector(".rock");
-const paper = document.querySelector(".paper");
-const scissors = document.querySelector(".scissors");
-const updateScore = document.querySelector("#score");
-const playAgain = document.querySelector(".play-again");
+
+let rulesBtn = document.querySelector(".rules-btn");
+let closeBtn = document.querySelector(".cross");
+let playAgainBtn = document.querySelector(".play-again");
+let options = document.querySelector(".options");
+let choices = document.querySelector(".choices");
+let showResult = document.querySelector(".show-result");
+let result = document.querySelector(".result");
+let userPara = document.querySelector(".user-para");
+let comPara = document.querySelector(".com-para");
+let userChoiceElements = [...document.querySelectorAll(".user-choice")];
+let option = [...document.querySelectorAll(".option")];
+let rock = document.querySelector(".rock");
+let paper = document.querySelector(".paper");
+let scissors = document.querySelector(".rules");
+let score = document.querySelector(".score");
+
+// funtion definations
+
+function toggleRules(){
+    let rules = document.querySelector(".rules-img");
+    rules.classList.toggle("hide");
+}
+let scoreNumber = Number(score.textContent);
+
+function playRound(playerSelection, computerSelection){
+    let winnerLoser = document.querySelector(".result p");
+    
+    if(playerSelection === "paper" && computerSelection === "rock"){
+        winnerLoser.textContent = "You win";
+        scoreNumber +=1;
+        score.textContent = scoreNumber;
+    }
+    else if(playerSelection === "scissors" && computerSelection === "paper"){
+        winnerLoser.textContent = "You win";
+        scoreNumber +=1;
+        score.textContent = scoreNumber;
+    }
+    else if(playerSelection === "rock" && computerSelection === "scissors"){
+        winnerLoser.textContent = "You win";
+        scoreNumber +=1;
+        score.textContent = scoreNumber;
+    }
+    else if(playerSelection === "paper" && computerSelection === "scissors"){
+        winnerLoser.textContent = "You Lose";
+    }
+    else if(playerSelection === "rock" && computerSelection === "paper"){
+        winnerLoser.textContent = "You Lose";
+    }
+    else if(playerSelection === "scissors" && computerSelection === "rock"){
+        winnerLoser.textContent = "You Lose";
+    }
+    else{
+        winnerLoser.textContent = "Draw";
+    }
+}
+// computer random choices
+let comChoice = document.querySelector(".computer-choice");
+function computerChoice(){
+    // choose random styles from the three of hands
+    let i = Math.floor(Math.random()* option.length);
+    comChoice.firstElementChild.classList.add(option[i].classList[0], "option");
+    comChoice.firstElementChild.firstElementChild.src = option[i].firstElementChild.src;
+    return (option[i].classList[0])
+
+}
+let userChoice = document.querySelector(".user-choice");
+function showUserSelection(item){
+    userChoice.firstElementChild.classList.add(item.classList[0], "option");
+    userChoice.firstElementChild.firstElementChild.src = item.firstElementChild.src;
+    return item.classList[0];
+}
+
+function showSelection(e){
+    if(option.includes(e.target)){
+        options.classList.toggle("hide");
+        showResult.classList.toggle("hide");
+    }
+    // computerChoice();
+    showUserSelection(e.target);
+    let computChoice = computerChoice();
+    let userSelection = showUserSelection(e.target);
+    playRound(userSelection, computChoice);
+}
+function playAgain(){
+    comChoice.firstElementChild.classList = "";
+    userChoice.firstElementChild.classList = "";
+    options.classList.toggle("hide");
+    showResult.classList.toggle("hide");
+}
 
 
-// show and hide rules 
+// show and hide game rules
+rulesBtn.addEventListener("click", toggleRules);
+closeBtn.addEventListener("click", toggleRules);
 
-ruleBtn.addEventListener("click", (e)=>{
-    displayRules.style.zIndex = "9";
-    displayRules.style.opacity = "1";
+// show user and computer Selections
+options.addEventListener("click", showSelection);
 
-})
-closeRules.addEventListener("click", (e)=>{
-    displayRules.style.zIndex = "-10";
-    displayRules.style.opacity = "0";
-
-})
-
-// show selection
-
-display.addEventListener("click", (e)=>{
-    choiceBtns.classList.add("hide");
-    selection.classList.remove("hide");
-
-})
-
-// hide selection
-playAgain.addEventListener("click", (e)=>{
-    choiceBtns.classList.remove("hide");
-    selection.classList.add("hide");
-})
+playAgainBtn.addEventListener("click", playAgain)
